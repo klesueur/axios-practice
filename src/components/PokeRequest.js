d02f0602be8ecd6fee7cbe2d57f56fc8e8bf9070
Kara LeSueur <kara.lesueur@gmail.com>
2021-03-12 14:46:37 -0800

    loading issues

--- src/components/PokeRequest.js
+++ src/components/PokeRequest.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import Axios from 'axios';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 
 const PokeRequest = () => {
+    const [pokeArr, setPokeArr] = useState([]);
 
+    const randomPokemon = () => {
+        const randomNum = Math.ceil(Math.random() * 250);
+
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
+            .then((res) => {
+                const tempArr = [...pokeArr];
+
+                tempArr.push(res.data);
+                setPokeArr([...tempArr]);
+                console.log(pokeArr)
+            });
+    };
 
     return (
         <div className='pokerequest-view'>
-
+            <button className='random-btn' onClick={randomPokemon()}> Get Random Pokemon! </button>
+            
         </div>
     );
 };
