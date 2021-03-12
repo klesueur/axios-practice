78c8cf619e6372e1cced2df7511bd9fb68bcfdba
Kara LeSueur <kara.lesueur@gmail.com>
2021-03-12 15:58:55 -0800

    fine tuming data

--- src/components/PokeRequest.js
+++ src/components/PokeRequest.js
@@ -1,18 +1,11 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Pokemon from './Pokemon';
 
 
 const PokeRequest = () => {
     const [pokeArr, setPokeArr] = useState([]);
 
-    // useEffect(() => {
-    //     axios.get(`https://pokeapi.co/api/v2/pokemon/78`)
-    //         .then(res => {
-    //             console.log(res)
-    //         }).catch(err => {
-    //             console.log(err)
-    //         })
-    // }, []);
 
     const randomPokemon = () => {
         const randomId = Math.ceil(Math.random() * 250);
@@ -20,15 +13,22 @@ const PokeRequest = () => {
         axios.get(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
             .then((res) => {
                 console.log(res)
+                const tempArr = [...pokeArr]
+                tempArr.push(res.data)
+                setPokeArr([...tempArr])
             }).catch((err) => {
                 console.log(err)
             })
     };
 
+    const mappedPokemon = pokeArr.map((pokemon, index) => {
+        return <Pokemon pokemon={pokemon} />
+    });
+
     return (
         <div className='pokerequest-view'>
             <button className='random-btn' onClick={() => randomPokemon()}> Get Random Pokemon! </button>
-            <p></p>
+            <div></div>
         </div>
     ); 
 };
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 
-const Pokemon = () => {
-
+const Pokemon = (props) => {
+    const {sprites, name} = props.pokemon;
 
     return (
         <div className='pokemon-view'>
-
+            <h2>{name}</h2>
+            <img src={sprites.back_default} />
+            <img src={sprites.front_default} />
         </div>
     );
 };
