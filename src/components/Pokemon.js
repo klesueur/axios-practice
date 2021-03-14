fa6b362cc7e1fc7aa8ec9190abf89ae7434f13df
Kara LeSueur <kara.lesueur@gmail.com>
2021-03-13 19:47:04 -0800

    upgraded images

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -3,19 +3,28 @@ import React from 'react';
 
 const Pokemon = (props) => {
     const {id, name, sprites, types} = props.pokemon;
+    console.log('props pokemon', props.pokemon)
 
     return (
         <div className='pokemon-card'>
             <div className='card-left-side'>
                 <div className='card-header'>
-                    <h4>#{id}</h4>
-                    <h2>{name}</h2>
+                    <span className='main-identifiers'>
+                        <p className='id-num'>#{id}</p>
+                        <p className='divider'> | </p>
+                        <h4 className='name'>{name}</h4>
+                    </span>
+                </div>
+                <div className='card-description'>
+
                 </div>
             </div>
             <div className='card-right-side'>
                 <div className='pokemon-imgs'>
-                    <img src={sprites.front_default} />
-                    {/* <img src={sprites.back_default} /> */}
+                    <img src={sprites.other["official-artwork"].front_default} />
+                </div>
+                <div className='chip-types'>
+                    
                 </div>
             </div>
         </div>
